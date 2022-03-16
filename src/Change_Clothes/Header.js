13dd5d509a48c8875a8cbaee1d8db80d7ff567fe
Tuandao1991 <rickytuan1991@gmail.com>
2022-03-17 00:57:25 +0200

    Version - 17/3/2022

--- src/Change_Clothes/Header.js
+++ src/Change_Clothes/Header.js
@@ -7,12 +7,11 @@ export default function Header() {
             <div className="col-sm-12">
                 <div className="card">
                     <div className="text-center">
-                        <img src="../assets/images/cybersoft.png" alt="Card image" />
                     </div>
                     <div className="card-body">
                         <h4 className="card-title text-center">
-                            CyberLearn - Học lập trình trực tuyến - Dự án thử đồ trực tuyến
-                            - Virtual Dressing Room
+                            CyberLearn -
+                            Virtual Dressing Room
                         </h4>
                     </div>
                 </div>
